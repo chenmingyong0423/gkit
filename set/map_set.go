9a0ebb626794fca7a16346d08b1a57ac0273d685
chenmingyong <chenmingyong5873@126.com>
2023-02-24 16:49:29 +0800

    - map：新增将切片转换成 map 函数 - set: 添加函数注释 - 编写新增函数的测试用例

--- set/map_set.go
+++ set/map_set.go
@@ -20,33 +20,40 @@ type MapSet[T comparable] struct {
 	mp map[T]struct{}
 }
 
+// NewMapSet 创建指定大小的 MapSet
 func NewMapSet[T comparable](size int) MapSet[T] {
 	return MapSet[T]{
 		mp: make(map[T]struct{}, size),
 	}
 }
 
+// Add 添加一个元素
 func (ms *MapSet[T]) Add(val T) {
 	ms.mp[val] = struct{}{}
 }
 
+// Remove 删除一个元素
 func (ms *MapSet[T]) Remove(val T) {
 	delete(ms.mp, val)
 }
 
+// Contains 判断 MapSet 是否包含 val
 func (ms *MapSet[T]) Contains(val T) bool {
 	_, ok := ms.mp[val]
 	return ok
 }
 
+// IsEmpty 判断 MapSet 是否为空
 func (ms *MapSet[T]) IsEmpty() bool {
 	return len(ms.mp) == 0
 }
 
+// Size 获取 MapSet 的大小
 func (ms *MapSet[T]) Size() int {
 	return len(ms.mp)
 }
 
+// Clear 清空 MapSet
 func (ms *MapSet[T]) Clear() {
 	if len(ms.mp) != 0 {
 		ms.mp = map[T]struct{}{}
