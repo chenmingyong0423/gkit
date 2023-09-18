51b7a48c55e5cffac895142b1bb63e7a1f859783
chenmingyong <chenmingyong5873@126.com>
2023-09-18 17:34:39 +0800

    gkit: 新增函数 GetValueOrDefault → 获取指针指向的值，如果为 nil，则返回默认值

--- ptr.go
+++ ptr.go
@@ -18,3 +18,12 @@ package gkit
 func ToPtr[T any](t T) *T {
 	return &t
 }
+
+// GetValueOrDefault 获取指针指向的值，如果为 nil，则返回默认值
+// 注意：只会取第一层指针所指向的值
+func GetValueOrDefault[T any](src *T) (t T) {
+	if src != nil {
+		t = *src
+	}
+	return
+}
--- ptr_test.go
+++ ptr_test.go
@@ -24,3 +24,10 @@ func TestToPtr(t *testing.T) {
 	v := 1
 	assert.Equal(t, &v, ToPtr(v))
 }
+
+func TestGetValueOrDefault(t *testing.T) {
+	val := new(int)
+	*val = 1
+	assert.Equal(t, *val, GetValueOrDefault(val))
+	assert.Equal(t, "", GetValueOrDefault[string](nil))
+}
